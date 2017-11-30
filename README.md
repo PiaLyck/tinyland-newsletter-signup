7da8866398e4708c35f154d6871c88740cc941fa
Pia Lyck Festersen <pialyck@gmail.com>
2017-11-30 14:59:02 +1100

    add Bootstrap v4, update README, add html sign up form

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## Tinyland.dk newsletter form
 Newsletter signup form with Node.js, Express.js, and the MailChimp API.
+
+# + 
+
+### Nodemon
+...for watching changes in Node files and stop and restart our Node server (Express)
+
+### Bootstrap v4.0
+...for quick, responsive, mobile-first styling
--- app.js
+++ app.js
@@ -4,3 +4,18 @@ Responsible for setting up our app,
 processing any POST requests sent to our server,
 and adding users emails to MailChimp.
 */
+
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.static(__dirname + '/public'))
+
+app.listen(3111, function () {
+    console.log('Listening on port 3111');
+});
+
+// Root
+/* app.get('/', function (req, res) {
+    res.end('yoooooo');
+}); */
